631ae01d84b6c0646951b5025dfcd3aa401b295c
tpeperomia <tpeperomia@gmail.com>
2024-08-27 19:32:24 +1000

    Redo with testing code

---
+++ 2password_gen.py
@@ -0,0 +1,32 @@
+#imported modules
+import string
+import secrets
+
+#User interface to request test password length
+def get_length():
+    requested_length = input("How many characters do you need? ")
+    
+#test input is int and convert
+    try : 
+        int(requested_length)
+        length = int(requested_length)
+
+#test for secure password length
+        validation = False if length < 10 else True
+        if validation == False :
+            print("Input is not valid, please pick more than 10 for safety.")
+            get_length()
+        else : 
+            print("Great, here is you password: ")
+            options = list(string.ascii_letters + string.digits + string.punctuation)
+            password = ''.join(secrets.choice(options) for i in range(length))
+            print(password)
+
+    except : 
+        print("Input is not valid, please use only numbers.") 
+        get_length()
+
+get_length()
+
+
+
